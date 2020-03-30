dddb4d720243cff92b7bfc1e0575cd03a944b2b2
Gabriela.Piovezan <gabrielapiovezan@live.com>
2020-03-30 19:23:22 -0300

    Criacao da estrutura html e deparacao dos programas js

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,27 @@
 const cipher = {
-  // ...
+nome:"Gabi"
 };
 
+
+
+
+operacao  (desl,texto)
+{
+let a =[]
+   for(let i=0;i<texto.length;i++){
+      a[i] = ((texto.charAt(i)).toUpperCase())
+      if(a[i].charCodeAt(0)<65  || a[i].charCodeAt(0)>90)
+      a[i]= a[i].charCodeAt(0)
+      else
+      {        
+         alfabetoDesl = (a[i].charCodeAt(0)-65) + desl
+         if(alfabetoDesl>=0)
+            a[i] = (alfabetoDesl)%26+65
+         else
+            a[i]= (Math.abs(alfabetoDesl)%26-91)*-1
+      }
+      console.log(String.fromCharCode(a[i]))
+   }
+return a
+} 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+
+let saida = document.getElementById("saida")
+document.getElementById('caixaDeTexto').value = '';
+
+function enviar()
+{  
+   saida.innerHTML = ``   
+   let escolha = document.getElementById("escolha")
+   let deslocamento = document.getElementById("deslocamento")
+   let texto= document.getElementById("caixaDeTexto").value
+   let desl = parseInt(deslocamento.value)
+   if(escolha.escolhaRadio.value=="d")
+   desl *=-1
+   //let resultado = cipher.funcao.operacao(desl,texto)
+
+   for(let i=0; i<resultado.length;i++)
+   {
+    saida.innerHTML += `${String.fromCharCode(resultado[i])}`  
+   }
+}
