21314e65283768e4eaae2abea0821009555c913d
Gabriela.Piovezan <gabrielapiovezan@live.com>
2020-04-10 03:14:18 -0300

    Atualização da estrutura, adicionando algumas funções no menu superior e tela de boas vindas

--- src/cipher.js
+++ src/cipher.js
@@ -25,8 +25,8 @@ const cipher = {
                let validateCondition = (messageArray[i].charCodeAt(0) - letter) + displacementNumber
                if (validateCondition % 26 >= 0)
                   messageArray[i] = (validateCondition) % 26 + letter
-               else   
-               messageArray[i] = ((Math.abs(validateCondition)) % 26 - spin) * -1
+               else
+                  messageArray[i] = ((Math.abs(validateCondition)) % 26 - spin) * -1
             }
             letter = 0;
             messageReturn += String.fromCharCode(messageArray[i])
@@ -36,34 +36,39 @@ const cipher = {
    },
 
    decode(displacementNumber, message) {
-      displacementNumber *= -1
-      let messageReturn = ""
-      let messageArray = []
-      let letter = 0
-      let spin = 91
-      for (let i = 0; i < message.length; i++) {
-         messageArray[i] = (message.charAt(i))
-         if (messageArray[i].charCodeAt(0) >= 97 && messageArray[i].charCodeAt(0) <= 122) {
-            letter = 97
-            spin = 123
+
+      if (!displacementNumber || !message)
+         throw new TypeError("Parametros não conferem");
+      else {
+         displacementNumber *= -1
+         let messageReturn = ""
+         let messageArray = []
+         let letter = 0
+         let spin = 91
+         for (let i = 0; i < message.length; i++) {
+            messageArray[i] = (message.charAt(i))
+            if (messageArray[i].charCodeAt(0) >= 97 && messageArray[i].charCodeAt(0) <= 122) {
+               letter = 97
+               spin = 123
+            }
+            if (messageArray[i].charCodeAt(0) >= 65 && messageArray[i].charCodeAt(0) <= 90) {
+               letter = 65
+               spin = 91
+            }
+            if (letter === 0)
+               messageArray[i] = (messageArray[i].charCodeAt())
+            else {
+               let validateCondition = (messageArray[i].charCodeAt(0) - letter) + displacementNumber
+               if (validateCondition % 26 >= 0)
+                  messageArray[i] = (validateCondition) % 26 + letter
+               else
+                  messageArray[i] = (Math.abs(validateCondition) % 26 - spin) * -1
+            }
+            letter = 0;
+            messageReturn += String.fromCharCode(messageArray[i])
          }
-         if (messageArray[i].charCodeAt(0) >= 65 && messageArray[i].charCodeAt(0) <= 90) {
-            letter = 65
-            spin = 91
-         }
-         if (letter === 0)
-            messageArray[i] = (messageArray[i].charCodeAt())
-         else {
-            let validateCondition = (messageArray[i].charCodeAt(0) - letter) + displacementNumber
-            if (validateCondition % 26 >= 0)
-               messageArray[i] = (validateCondition) % 26 + letter
-            else 
-               messageArray[i] = (Math.abs(validateCondition) % 26 - spin) * -1   
-         }
-         letter = 0;
-         messageReturn += String.fromCharCode(messageArray[i])
+         return messageReturn
       }
-      return messageReturn
    }
 };
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ let displacement = document.getElementById("displacement")
 document.getElementById("messageBox").value = '';
 
 
+
 const send = function () {
    let resultPrint
    result.innerHTML = ""
@@ -25,7 +26,34 @@ const send = function () {
    }
    result.innerHTML += `${resultPrint}`
 }
-
+const clear = function () {
+   document.getElementById("sobre").style.display = "none"
+   document.getElementById("sec").style.display = "none"
+   document.getElementById("welcome").style.display = "none"
+   document.getElementById("contatoEmail").style.display = "none"
+}
+const cifraReceita = function () {
+   clear()
+   document.getElementById("sobre").style.display = "block"
+}
+const inicio = function () {
+   clear()
+   document.getElementById("welcome").style.display = "block"
+}
+const start = function () {
+   clear()
+   document.getElementById("sec").style.display = "block"
+}
+const contato = function () {
+   clear()
+   document.getElementById("contatoEmail").style.display = "block"
+}
+clear()
+inicio()
 document.getElementById("messageBox").addEventListener("input", send);
 document.getElementById("choice").addEventListener("click", send);
 document.getElementById("displacement").addEventListener("click", send);
+document.getElementById("cifraReceita").addEventListener("click", cifraReceita)
+document.getElementById("inicio").addEventListener("click", inicio)
+document.getElementById("sim").addEventListener("click", start)
+document.getElementById("contato").addEventListener("click", contato)
