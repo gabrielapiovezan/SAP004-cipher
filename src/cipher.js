b8e1da2d14af92d7891891d1b163af13aed634ea
Gabriela.Piovezan <gabrielapiovezan@live.com>
2020-04-17 05:52:51 -0300

    Lógica do Cipher sem array

--- src/cipher.js
+++ src/cipher.js
@@ -1,48 +1,55 @@
 const cipher = {
-   encode (displacementNumber, message)
-   {
+   encode(displacementNumber, message) {
       const messageReturn = start(displacementNumber, message)
       return messageReturn;
    },
-   decode (displacementNumber, message)
-   {
+
+   decode(displacementNumber, message) {
       const messageReturn = start(-displacementNumber, message)
       return messageReturn;
    }
-  };
-  
-  const start = function(displacementNumber, message) {
-  
-     if (!displacementNumber || !message)
-        throw new TypeError("Parametros não conferem");
-     else {
-        let messageReturn = ""
-        let messageArray = []
-        let letter = 0
-        let spin = 91
-        for (let i = 0; i < message.length; i++) {
-           messageArray[i] = (message.charAt(i))
-           if (messageArray[i].charCodeAt(0) >= 97 && messageArray[i].charCodeAt(0) <= 122) {
-              letter = 97
-              spin = 123
-           }
-           if (messageArray[i].charCodeAt(0) >= 65 && messageArray[i].charCodeAt(0) <= 90) {
-              letter = 65
-              spin = 91
-           }
-           if (letter === 0)
-              messageArray[i] = (messageArray[i].charCodeAt())
-           else {
-              let validateCondition = (messageArray[i].charCodeAt(0) - letter) + displacementNumber
-              if (validateCondition % 26 >= 0)
-                 messageArray[i] = validateCondition % 26 + letter
-              else
-                 messageArray[i] = (Math.abs(validateCondition) % 26 - spin) * -1
-           }
-           letter = 0
-           messageReturn += String.fromCharCode(messageArray[i])
-        }
-        return messageReturn
-     }
-  }
-  export default cipher;
+};
+
+const start = function (displacementNumber, message) {
+   if (!displacementNumber || !message)
+      throw new TypeError("Parametros não conferem");
+
+   else {
+
+      let messageReturn = ""
+
+      let letter = 0
+      let spin = 91
+
+      for (let i = 0; i < message.length; i++) {
+
+         if ((message.charCodeAt(i) >= 97) && (message.charCodeAt(i) <= 122)) {
+
+            letter = 97
+            spin = 123
+         }
+
+         else if ((message.charCodeAt(i) >= 65) && (message.charCodeAt(i) <= 90)) {
+            letter = 65
+            spin = 91
+
+         }
+
+         if (letter === 0) {
+            messageReturn += String.fromCharCode(message.charCodeAt(i))
+
+         }
+         else {
+            let validateCondition = (message.charCodeAt(i) - letter) + displacementNumber
+            if (validateCondition % 26 >= 0)
+               messageReturn += String.fromCharCode(validateCondition % 26 + letter)
+            else
+               messageReturn += String.fromCharCode((Math.abs(validateCondition) % 26 - spin) * -1)
+         }
+         letter = 0
+
+      }
+      return messageReturn
+   }
+}
+export default cipher;
